ac1090021c1e19fcbaf0dd0cd60f720012b1ca28
puspitasahu <puspita5sahu@gmail.com>
2026-01-05 14:03:27 +0530

    Added Configuration for CORS

---
+++ src/main/java/com/dailywork/aicustomersupport/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.dailywork.aicustomersupport.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.constraints.NotNull;
+
+@Configuration
+public class AppConfig {
+
+   @Bean
+   public WebMvcConfigurer corsConfigurer(){
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
