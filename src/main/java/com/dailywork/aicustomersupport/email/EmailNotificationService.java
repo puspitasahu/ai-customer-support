27a850c724e121cee5ab4734db9d523f0bc86145
puspitasahu <puspita5sahu@gmail.com>
2026-01-08 11:17:23 +0530

    adding EmailNotificationService

--- src/main/java/com/dailywork/aicustomersupport/email/EmailNotificationService.java
+++ src/main/java/com/dailywork/aicustomersupport/email/EmailNotificationService.java
@@ -1,33 +1,32 @@
 package com.dailywork.aicustomersupport.email;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
+import com.dailywork.aicustomersupport.model.Customer;
+import com.dailywork.aicustomersupport.model.Ticket;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Component;
-
-import java.io.UnsupportedEncodingException;
-
-import static com.dailywork.aicustomersupport.email.EmailProperties.DEFAULT_USERNAME;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Slf4j
-@Component
+@Service
 public class EmailNotificationService {
-    private final JavaMailSender mailSender;
+    private  final EmailService emailService;
 
-    public void sendNotificationEmail(String to,String subject,String senderName,String mailContent) throws MessagingException, UnsupportedEncodingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        var messageHelper = new MimeMessageHelper(message,true,"UTF_8");
-        messageHelper.setFrom(DEFAULT_USERNAME,senderName);
-        messageHelper.setTo(to);
-        messageHelper.setSubject(subject);
-        messageHelper.setText(mailContent,true);
-        mailSender.send(message);
-        log.info("Email message sent successfully : {}",message);
+    public void sendTicketNotificationEmail(Ticket ticket) {
+        Customer customer =null;
+        String customerName = null;
+        String customerEmail = null;
+        String senderName="Customer Support Service";
+        String subject="Support Ticket Created";
+        String body="";
+        try{
+            emailService.sendNotificationEmail(customerEmail,subject,senderName,body);
+        }catch(Exception e){
+            throw new RuntimeException(("Failed to send notification email"));
+        }
+
 
 
     }
+
 }
--- src/main/java/com/dailywork/aicustomersupport/email/EmailService.java
+++ src/main/java/com/dailywork/aicustomersupport/email/EmailService.java
@@ -1,4 +1,31 @@
 package com.dailywork.aicustomersupport.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+
+import static com.dailywork.aicustomersupport.email.EmailProperties.DEFAULT_USERNAME;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
 public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void sendNotificationEmail(String to, String subject, String senderName, String mailContent) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message, true, "UTF_8");
+        messageHelper.setFrom(DEFAULT_USERNAME, senderName);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(mailContent, true);
+        mailSender.send(message);
+        log.info("Email message sent successfully : {}", message);
+    }
 }
