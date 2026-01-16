a0989293d209ed4ebb97d560786cd6eee92945d2
puspitasahu <puspita5sahu@gmail.com>
2026-01-16 11:31:57 +0530

    Added Email Notification Template and updated the temaplte in EmailNotificationService

--- src/main/java/com/dailywork/aicustomersupport/email/EmailNotificationService.java
+++ src/main/java/com/dailywork/aicustomersupport/email/EmailNotificationService.java
@@ -3,9 +3,17 @@ package com.dailywork.aicustomersupport.email;
 import com.dailywork.aicustomersupport.model.Customer;
 import com.dailywork.aicustomersupport.model.Ticket;
 import com.dailywork.aicustomersupport.service.customer.CustomerService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -30,17 +38,32 @@ public class EmailNotificationService {
 
             String customerName = customer.getFullName();
             String customerEmail = customer.getEmailAddress();
+            String customerPhone = customer.getPhoneNumber();
+
+            String ticketDetails = ticket.getConversation().getConversationSummary();
+            String ticketTitle = ticket.getConversation().getConversationTitle();
+            String referenceNumber = ticket.getReferenceNumber();
 
             log.info("   Customer: {}", customerName);
             log.info("   Email: {}", customerEmail);
 
             String senderName = "Customer Support Service";
             String subject = "Support Ticket Created";
-            String body = getHtmlBody(ticket, customerName, senderName);
+            String htmlBody = null;
 
-            log.info(" Calling emailService.sendNotificationEmail()...");
-            emailService.sendNotificationEmail(customerEmail, subject, senderName, body);
-            log.info(" Email sent successfully!");
+           try{
+               htmlBody = loadEmailTemplate(customerName,customerEmail,customerPhone,ticketDetails,ticketTitle,referenceNumber);
+           }catch(Exception e){
+            log.error("Error loading email template: ",e);
+           }
+           try{
+               log.info(" Calling emailService.sendNotificationEmail()...");
+               emailService.sendNotificationEmail(customerEmail, subject, senderName, htmlBody);
+               log.info(" Email sent successfully!");
+           }catch(MessagingException | UnsupportedEncodingException e){
+               log.error("Error sending email notification:",e);
+           }
+
 
         } catch (Exception e) {
             log.error("════════════════════════════════════════");
@@ -57,7 +80,19 @@ public class EmailNotificationService {
         log.info("════════════════════════════════════════");
     }
 
-    private static String getHtmlBody(Ticket ticket, String userName, String senderName) {
+    private String loadEmailTemplate(String customerName,String customerEmail,String customerPhone,String ticketDetails,String ticketTitle,String referenceNumber) throws IOException {
+        ClassPathResource resource = new ClassPathResource("templates/ticket-notification-template.html");
+        String template = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+        template = template.replace("{{customerName}}", customerEmail);
+        template = template.replace("{{customerEmail}}",customerEmail);
+        template = template.replace("{{customerPhone}}",customerPhone);
+        template = template.replace("{{ticketDetails}}",ticketDetails);
+        template = template.replace("{{ticketTitle}}",ticketTitle);
+        template = template.replace("{{ticketReferenceNumber}}",referenceNumber);
+        return template;
+    }
+
+    /*private static String getHtmlBody(Ticket ticket, String userName, String senderName) {
         String ticketDetails = ticket.getConversation().getConversationSummary();
         String ticketTitle = ticket.getConversation().getConversationTitle();
         String referenceNumber = ticket.getReferenceNumber();
@@ -78,5 +113,5 @@ public class EmailNotificationService {
                     </body>
                 </html>
                 """.formatted(userName, referenceNumber, ticketTitle, ticketDetails, senderName);
-    }
+    }*/
 }
